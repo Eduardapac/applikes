78c3c84b89505bf1a75a6cd2eec925c21ed19dd4
Eduardapac <eduardaleitepac@gmail.com>
2024-06-25 10:34:46 -0300

    aaaaaaaaaaaaaaaaa

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import {Post} from "./Post"
+
+export function App() {
+return (
+  <div>
+    <Post 
+      author="MAchado de Assis"
+      coment="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem, nesciunt. Explicabo voluptate minima libero aperiam velit autem,
+       voluptatibus quasi dicta tempora sequi labore dolore, voluptatem distinctio corrupti officiis optio quas!"/>
+    <Post 
+      author="Clarice Lispector"
+      coment="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem, nesciunt. Explicabo voluptate minima libero aperiam velit autem,
+       voluptatibus quasi dicta tempora sequi labore dolore, voluptatem distinctio corrupti officiis optio quas!"/>
+    <Post 
+      author="Jane Austin"
+      coment="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem, nesciunt. Explicabo voluptate minima libero aperiam velit autem,
+       voluptatibus quasi dicta tempora sequi labore dolore, voluptatem distinctio corrupti officiis optio quas!"/>
+  </div>
+  )
+}
